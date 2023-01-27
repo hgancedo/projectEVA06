09a7f3b60e17d45124faa2f72febef9f8b901167
Hector Gancedo Grade <hgancedo@birt.eus>
2023-01-27 18:22:13 +0100

    Clase Operaciones en server->setClass puesto a mayúscula

--- serversoap/serverfile.php
+++ serversoap/serverfile.php
@@ -3,13 +3,12 @@
 require "../src/operaciones.php";
 //use SoapServer
 //use SoapFault?
-
 $uri ="http://localhost/UD06/projectEVA06/serversoap";
 $param = ['uri' => $uri];
 
 try {
     $server = new SoapServer(null, $param);
-    $server->setClass('operaciones');
+    $server->setClass('Operaciones');
     $server->handle();
 } catch (SoapFault $ex) {
     die("error en soap server: " .$ex->getMessage());
